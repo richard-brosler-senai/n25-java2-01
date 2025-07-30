7f9acba545f650fade99f1ae4899665ab6afbe06
Richard Brosler <richard.brosler@sp.senai.br>
2025-07-29 21:57:18 -0300

    Aula 02

---
+++ Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao02/aula02/Text01.java
@@ -0,0 +1,16 @@
+package br.com.senaisp.bauru.secao02.aula02;
+
+public class Text01 {
+    public static void main(String[] args) {
+        System.out.println("   /\\         /\\   "); 
+        System.out.println("  /  \\_****__/  \\  ");
+        System.out.println(" /               \\ ");
+        { 
+        	System.out.println("(  /\\         /\\  )");
+        	System.out.println("====     V     ====");
+        	System.out.println("======(__|__)======");
+        }
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+    }    
+}
