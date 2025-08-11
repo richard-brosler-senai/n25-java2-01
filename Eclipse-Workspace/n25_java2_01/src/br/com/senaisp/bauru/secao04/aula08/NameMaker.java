fff77422d9304166bd57f2bb8059846956cdb02c
Richard Brosler <richard.brosler@sp.senai.br>
2025-08-11 18:42:48 -0300

    Ajustes

---
+++ Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao04/aula08/NameMaker.java
@@ -0,0 +1,18 @@
+package br.com.senaisp.bauru.secao04.aula08;
+
+import javax.swing.JOptionPane;
+
+public class NameMaker {
+
+	public static void main(String[] args) {
+		//Solicitar o nome, nome do meio e sobrenome separadamente
+		//Mostrar o nome completo ao término
+		//pode usar o Scanner ou JoptionPane para solicitar ao usuário
+		String nome = JOptionPane.showInputDialog("Digite seu nome: ");
+		String nomeMeio = JOptionPane.showInputDialog("Agora nome do meio: ");
+		String sobrenome = JOptionPane.showInputDialog("Agora sobrenome: ");
+		String nomeCompleto = nome + " " + nomeMeio + " " + sobrenome;
+		JOptionPane.showMessageDialog(null, "Seu nome completo é " + nomeCompleto);
+	}
+
+}
