ee6d57ef82c84b52530979481dd05bca3ccf8262
Richard Brosler <richard.brosler@sp.senai.br>
2025-09-09 19:03:02 -0300

    Conteudo da aula do dia 08-09-2025

---
+++ Eclipse-Workspace/projeto_javafx_module/src/module-info.java
@@ -0,0 +1,8 @@
+module projeto_javafx_module {
+	requires javafx.graphics;
+	requires javafx.media;
+	requires javafx.controls;
+	
+	opens br.com.senaisp.bauru.classes to javafx.graphics,
+							javafx.media, javafx.controls;
+}
